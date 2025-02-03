cfa78be0a0395ec3441676ce8d5137fbfce3d648
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2025-02-03 10:18:13 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#calculate-btn").click(function () {
+        let hours = $("#hours").val();
+        let rate = $("#rate").val();
+
+        if (hours > 0) {
+            $("#error-message").hide();
+            let total = hours * rate;
+            $("#total").val(total.toFixed(2));
+        } else {
+            $("#error-message").show();
+            $("#total").val("");
+        }
+    });
+});
