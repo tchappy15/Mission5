675c1a0c953ced7bfbb09ec76d9b84ff85e4eccd
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2025-02-03 10:25:12 -0700

    added comments

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -3,17 +3,17 @@
 
 // Write your JavaScript code.
 
-$(document).ready(function () {
-    $("#calculate-btn").click(function () {
+$(document).ready(function () { //ensure that the code inside it runs only after the DOM has fully loaded
+    $("#calculate-btn").click(function () { //calculate rate * hours in this function
         let hours = $("#hours").val();
         let rate = $("#rate").val();
 
         if (hours > 0) {
             $("#error-message").hide();
             let total = hours * rate;
-            $("#total").val(total.toFixed(2));
+            $("#total").val(total.toFixed(2)); //2 decimal points
         } else {
-            $("#error-message").show();
+            $("#error-message").show(); //show error message
             $("#total").val("");
         }
     });
